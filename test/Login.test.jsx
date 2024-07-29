85da3a3864a846bac925d367ca8dfd9f820d23eb
iriyamakotoe <iriyamakotoe@icloud.com>
2024-07-30 04:59:26 +0900

    02_単体テストを作ろう

---
+++ test/Login.test.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen } from "@testing-library/react"
+import { expect, test, describe } from "vitest"
+import { Login } from "../src/pages/Login"
+
+
+describe("ログイン画面のレンダリング", () => {
+  test("ログイン画面のレンダリング", () => {
+    render(<Login />);
+
+    const title = screen.getByRole("heading", { level: 2 });
+    expect(title).toHaveTextContent("ログイン");
+
+    const emailInput = screen.getByLabelText("email");
+    expect(emailInput).toHaveTextContent("メールアドレス");
+
+    const passwordInput = screen.getByLabelText("password");
+    expect(passwordInput).toHaveTextContent("パスワード");
+
+    const button = screen.getByRole("button");
+    expect(button).toHaveTextContent("ログイン");
+  });
+});
