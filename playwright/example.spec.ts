23178f3afc7d30152add544bf4eb44095b1af865
iriyamakotoe <iriyamakotoe@icloud.com>
2024-07-30 04:59:16 +0900

    01_ブラウザテストを作ろう

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -1,27 +1,34 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+  await page.goto('http://localhost:5173/login')
 })
 
-test('emailのtype属性がemail,値が必須になっているかどうか', async ({ page }) => {
-  const emailInput = await page.getByLabel("email")
-  await expect(emailInput).toHaveAttribute('type','email');
-  await expect(emailInput).toHaveAttribute('required','');
+test('emailのtype属性がemailになっているかどうか', async ({ page }) => {
+  const emailInput = await page.locator("input[name='email']")
+  await expect(emailInput).toHaveAttribute('type','email')
 });
 
-test('passwordのtype属性がpassword,値が必須になっているかどうか', async ({ page }) => {
-  const passwordInput = await page.getByLabel("password")
-  await expect(passwordInput).toHaveAttribute('type','password');
-  await expect(passwordInput).toHaveAttribute('required','');
+test('passwordのtype属性がpasswordになっているかどうか', async ({ page }) => {
+  const passwordInput = await page.locator("input[name='password']")
+  await expect(passwordInput).toHaveAttribute('type','password')
 });
 
-test('emailへ値を入力して判定する', async ({ page }) => {
-  await page.getByLabel("email").fill('iriyama@icloud.com')
-  await expect(page.getByLabel("email")).toHaveValue('iriyama@icloud.com');
+test('emailの入力値に不備がある場合にエラーメッセージがでることの確認（不備がない時はエラーが出ないことの確認）', async ({ page }) => {
+  await page.locator("input[name='email']").fill('iriyamakotoe@icloud.com')
+  await page.getByRole('button', { name: /ログイン/i }).click()
+  await expect(page.locator(".email-error")).toHaveText('')
 });
 
-test('パスワードへ値を入力して判定する', async ({ page }) => {
-  await page.getByLabel("password").fill('kotoe')
-  await expect(page.getByLabel("password")).toHaveValue('kotoe');
+test('passwordの入力値に不備がある場合にエラーメッセージがでることの確認（不備がない時はエラーが出ないことの確認）', async ({ page }) => {
+  await page.locator("input[name='password']").fill('iriyamaaaaaaaaaaaaaaa')
+  await page.getByRole('button', { name: /ログイン/i }).click()
+  await expect(page.locator(".password-error")).toHaveText('')
 });
+
+// test('ログインが失敗した場合にエラーメッセージがでることの確認（成功する時はエラーが出ないことの確認）', async ({ page }) => {
+//   await page.locator("input[name='email']").fill('iriyamakotoe@icloud.com')
+//   await page.locator("input[name='password']").fill('iriyamaaaaaa')
+//   await page.getByRole('button', { name: /ログイン/i }).click()
+//   await expect(page.locator(".form-error")).toHaveText('')
+// });
