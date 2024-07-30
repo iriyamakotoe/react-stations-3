1e3f0aa5b69d705b945e946af9393305a564a674
iriyamakotoe <iriyamakotoe@icloud.com>
2024-07-30 14:29:36 +0900

    05_ページネーションを作ろう

---
+++ src/components/Pagenation.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react'
+import { useRecoilState } from 'recoil'
+import { counterAtom } from "../store/atom";
+import "./pagenation.scss";
+
+export const Pagenation = () => {
+  const prevBtnRef = useRef(null)
+  const [counter, setCounter] = useRecoilState(counterAtom)
+
+  useEffect(() => {
+    prevBtnRef.current.disabled = true;
+  }, [])
+
+  const handlePrevPage = () => {
+    if(counter >= 1) {
+      setCounter((c) => c - 1)
+      if(counter == 1) {
+        prevBtnRef.current.disabled = true;
+      }
+    }
+  }
+  const handleNextPage = () => {
+    setCounter((c) => c + 1)
+    prevBtnRef.current.disabled = false;
+  }
+  return (
+    <>
+    <div className="flex justify-between">
+      <button type='button' onClick={handlePrevPage} ref={prevBtnRef}>前へ</button>
+      <button type='button' onClick={handleNextPage}>次へ</button>
+    </div>
+    </>
+  );
+};
+
+export default Pagenation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { useRecoilState } from 'recoil'
-import { counterAtom, disableAtom } from "../store/atom";
-import { ReviewList } from "../components/ReviewList";
-import { PageControl } from "../components/PageControl";
+import { counterAtom } from "../store/atom";
+import { ReviewList } from "../components/ReviewList"
+import { Pagenation } from "../components/Pagenation";
 import "./home.scss";
 
 export const Home = () => {
   const [review, setReview] = useState([])
-  const [counter, setCounter] = useRecoilState(counterAtom)
+  const [counter, ] = useRecoilState(counterAtom)
   const fetchReview = () => {
     console.log(counter)
     fetch('https://railway.bookreview.techtrain.dev/public/books?offset=' + (counter*10))
@@ -20,21 +20,12 @@ export const Home = () => {
     fetchReview()
   }, [counter])
 
-  const handlePagePrev = () => {
-    if(counter >= 1) {
-      setCounter((c) => c - 1)
-    }
-  }
-  const handlePageNext = () => {
-      setCounter((c) => c + 1)
-  }
-
   return (
     <>
       <main>
       <h2 className="page-title">レビュー一覧</h2>
       <ReviewList review={review} />
-      <PageControl handlePagePrev={() => handlePagePrev()} handlePageNext={() => handlePageNext()} />
+      <Pagenation />
       </main>
     </>
   );
