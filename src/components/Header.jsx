dd19e69a4f7b191586aabc431f182ec90a1b4ab3
iriyamakotoe <iriyamakotoe@icloud.com>
2024-07-30 15:57:57 +0900

    04_書籍レビューの一覧画面を作ろう

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export const Header = () => {
   return (
     <header className="header">
-      <h1>書籍レビューアプリ</h1>
+      <h1><Link to="/">書籍レビューアプリ</Link></h1>
       <ul>
         <li><Link to="/signup">新規会員登録</Link></li>
         <li><Link to="/login">ログイン</Link></li>
